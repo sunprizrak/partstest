e87ce0eaa4048bfddbaf89b9a234fa0304ddf497
sunprizrak <sunprizrak@gmail.com>
2024-07-29 16:37:36 +0300

    ...

--- src/main.py
+++ src/main.py
@@ -19,55 +19,24 @@ class Catalog:
         resp = requests.get(
             f"{self.api_url}/{self.name}/catalog/tree"
         )
-
-        if resp.status_code == 200:
-            categories = resp.json().get('data')
-            for category in categories:
-                category_id = category.get('id')
-                obj = Category(category_id=category_id)
-                self.categories.append(obj)
-
         return resp
 
     def get_category(self, category_id):
         resp = requests.get(
             f"{self.api_url}/{self.name}/catalog/{category_id}"
         )
-
-        if resp.status_code == 200:
-            category = self.get_local_category(category_id=category_id)
-            data = resp.json().get('data')
-
-            for el in data:
-                children = el.get('children')
-
-                for part_list in children:
-                    external_id = part_list.get('external_id')
-                    category.parts_list.append(external_id)
-
-                    self.get_parts(category=category, parts_list_external_id=external_id)
-
         return resp
 
-    def get_parts(self, category, parts_list_external_id):
+    def get_parts(self, external_id):
         resp = requests.get(
-            f"{self.api_url}/{self.name}/catalog/{parts_list_external_id}/parts"
+            f"{self.api_url}/{self.name}/catalog/{external_id}/parts"
         )
-
-        if resp.status_code == 200:
-            data = resp.json().get('data')
-
-            for part in data:
-                part_id = part.get('id')
-                category.parts.append(part_id)
-
         return resp
 
     def get_part(self, part_id):
         resp = requests.get(
             f"{self.api_url}/{self.name}/part/{part_id}"
         )
-
         return resp
 
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
 import pytest
-from src.main import Catalog
-
+from src.main import Catalog, Category
 
 catalog_names = ['kubota']    # 'grimme'
 
@@ -11,20 +10,69 @@ for catalog_name in catalog_names:
 @pytest.mark.parametrize('catalog', Catalog.objects)
 class TestCatalog:
 
+    @pytest.mark.dependency(name="test_tree")
     def test_tree(self, catalog):
         response = catalog.get_tree()
-        assert response.status_code == 200
 
+        if response.status_code == 200:
+            categories = response.json().get('data')
+            if categories:
+                for category in categories:
+                    category_id = category.get('id')
+                    obj = Category(category_id=category_id)
+                    catalog.categories.append(obj)
+            else:
+                assert categories, 'Note Categories'
+        else:
+            assert response.status_code == 200, f'Bad request {response.status_code}'
+
+    @pytest.mark.dependency(depends=["test_tree"])
     def test_category(self, catalog):
         for category in catalog.categories:
             response = catalog.get_category(category_id=category.id)
-            assert response.status_code == 200
 
+            if response.status_code == 200:
+                data = response.json().get('data')
+                if data:
+                    for el in data:
+                        children = el.get('children')
+                        if children:
+                            for part_list in children:
+                                external_id = part_list.get('external_id')
+                                category.parts_list.append(external_id)
+
+                                response = catalog.get_parts(external_id=external_id)
+
+                                if response.status_code == 200:
+                                    data = response.json().get('data')
+
+                                    if data:
+                                        for part in data:
+                                            part_id = part.get('id')
+                                            category.parts.append(part_id)
+                                    else:
+                                        assert data, 'Note Part List Data'
+                                else:
+                                    assert response.status_code == 200, f'Bad request {response.status_code}'
+
+                        else:
+                            assert children, 'No children(Parts list) in category data'
+                else:
+                    assert data, 'Note Category Data'
+
+            else:
+                assert response.status_code == 200, f'Bad request {response.status_code}'
+
+    @pytest.mark.dependency(depends=["test_category"])
     def test_part(self, catalog):
         for category in catalog.categories:
             for part_id in category.parts:
                 response = catalog.get_part(part_id=part_id)
-                assert response.status_code == 200
+
+                if response.status_code == 200:
+                    pass
+                else:
+                    assert response.status_code == 200, f'Bad request {response.status_code}'
 
 
 
